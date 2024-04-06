a10901161b77739175779672e52d12b0b8f0196e
doulgs <douglasdsdomiciano@hotmail.com>
2024-04-06 15:53:12 -0300

    :construction: Adicionando Botao de Termos de Uso

--- src/screens/auth/SignUp/index.tsx
+++ src/screens/auth/SignUp/index.tsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import BouncyCheckbox from "react-native-bouncy-checkbox";
 
 import { Container, Scroll, Title } from "./styles";
+
 import { Input } from "../../../components/Input";
 import { Button } from "../../../components/Button";
+import { useTheme } from "styled-components/native";
 
 const SignUp: React.FC = () => {
+  const { Colors } = useTheme();
+
+  const [termos, setTermos] = useState<boolean>(false);
+
+  function handleCadastrar() {}
+
   return (
     <Scroll>
       <Container>
@@ -45,7 +54,25 @@ const SignUp: React.FC = () => {
           <Input.Icone name={"key-outline"} />
         </Input>
 
-        <Button title="Cadastrar" disabled />
+        <BouncyCheckbox
+          size={25}
+          fillColor={Colors.Primary[500]}
+          text="Li e concordo com os termos de uso"
+          iconStyle={{ borderColor: "red" }}
+          innerIconStyle={{ borderWidth: 2, borderRadius: 8 }}
+          textStyle={{
+            fontWeight: "bold",
+            textDecorationLine: "none",
+            color: Colors.Textcolor[700],
+          }}
+          onPress={(isChecked: boolean) => setTermos(isChecked)}
+        />
+
+        <Button
+          title="Cadastrar"
+          disabled={!termos}
+          onPress={handleCadastrar}
+        />
       </Container>
     </Scroll>
   );
