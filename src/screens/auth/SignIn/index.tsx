44285e7a2cce1f2f812feaa9e0d709d0229b0e6e
doulgs <douglasdsdomiciano@hotmail.com>
2024-04-06 12:39:32 -0300

    :construction: Tela de Login

--- src/screens/auth/SignIn/index.tsx
+++ src/screens/auth/SignIn/index.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 
 import { Container, Title } from "./styles";
+import { Input } from "../../../components/Input";
+import { Button } from "../../../components/Button";
 
 const SignIn: React.FC = () => {
   return (
     <Container>
-      <Title>Login</Title>
+      <Title>Entrar</Title>
+      <Input placeholder="Digite aqui seu email">
+        <Input.Icone name={"person"} />
+      </Input>
+      <Input placeholder="Digite aqui sua senha">
+        <Input.Icone name={"key"} />
+      </Input>
+      <Button title="Acessar" />
     </Container>
   );
 };
--- src/screens/auth/SignIn/styles.ts
+++ src/screens/auth/SignIn/styles.ts
@@ -2,8 +2,12 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
+  gap: ${(props) => props.theme.Size.md};
   padding: ${(props) => props.theme.Size.md};
   background-color: ${(props) => props.theme.Colors.Background[200]};
 `;
 
-export const Title = styled.Text``;
+export const Title = styled.Text`
+  text-align: center;
+  font-size: ${(props) => props.theme.Size.xl};
+`;
