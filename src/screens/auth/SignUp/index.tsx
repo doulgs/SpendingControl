1d847f47adbf935ffb793c2458668a45a1ec4a34
doulgs <douglasdsdomiciano@hotmail.com>
2024-04-06 12:39:39 -0300

    :construction: Tela de Cadastro

--- src/screens/auth/SignUp/index.tsx
+++ src/screens/auth/SignUp/index.tsx
@@ -1,9 +1,54 @@
 import React from "react";
 
-import { Container } from "./styles";
+import { Container, Scroll, Title } from "./styles";
+import { Input } from "../../../components/Input";
+import { Button } from "../../../components/Button";
 
 const SignUp: React.FC = () => {
-  return <Container />;
+  return (
+    <Scroll>
+      <Container>
+        <Title>Dados Pessoas</Title>
+        <Input placeholder="Digite aqui seu apelido" keyboardType="default">
+          <Input.Icone name={"person"} />
+        </Input>
+
+        <Input
+          placeholder="Digite aqui seu nome completo"
+          keyboardType="default"
+        >
+          <Input.Icone name={"person"} />
+        </Input>
+
+        <Input placeholder="Digite aqui seu telefone" keyboardType="number-pad">
+          <Input.Icone name={"phone-portrait"} />
+        </Input>
+
+        <Title>Cadastro</Title>
+        <Input placeholder="Digite aqui seu email" keyboardType="email-address">
+          <Input.Icone name={"mail"} />
+        </Input>
+
+        <Input
+          placeholder="Digite aqui sua senha"
+          keyboardType="default"
+          secureTextEntry
+        >
+          <Input.Icone name={"key"} />
+        </Input>
+
+        <Input
+          placeholder="Cofirme sua senha"
+          keyboardType="default"
+          secureTextEntry
+        >
+          <Input.Icone name={"key-outline"} />
+        </Input>
+
+        <Button title="Cadastrar" disabled />
+      </Container>
+    </Scroll>
+  );
 };
 
 export default SignUp;
--- src/screens/auth/SignUp/styles.ts
+++ src/screens/auth/SignUp/styles.ts
@@ -1,6 +1,17 @@
 import styled from "styled-components/native";
 
-export const Container = styled.View`
-  flex: 1;
+export const Scroll = styled.ScrollView`
   background-color: ${(props) => props.theme.Colors.Background[200]};
+`;
+
+export const Container = styled.View`
+  height: 100%;
+  gap: ${(props) => props.theme.Size.md};
+  padding: ${(props) => props.theme.Size.md};
+  background-color: ${(props) => props.theme.Colors.Background[200]};
+`;
+
+export const Title = styled.Text`
+  font-weight: bold;
+  font-size: ${(props) => props.theme.Size.md};
 `;
