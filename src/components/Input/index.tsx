63dea27dd26e59226195465ebb9dcf2228dd530a
doulgs <douglasdsdomiciano@hotmail.com>
2024-04-06 12:39:06 -0300

    :construction: Input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,22 +1,56 @@
-import React from "react";
-import { TextInputProps } from "react-native";
+import React, { ReactNode } from "react";
+import { StyleProp, TextInputProps, ViewStyle } from "react-native";
 import { useTheme } from "styled-components/native";
-import { Container, InputText } from "./styles";
+import { Container, ContainerIcon, InputText, Title } from "./styles";
+import { Ionicons } from "@expo/vector-icons";
 
-interface Props extends TextInputProps {}
+interface Props extends TextInputProps {
+  title?: string;
+  children?: ReactNode;
+  variant?: "SIMPLE" | "EXPOSED";
+  widthPercent?: number;
+}
 
-const Input: React.FC<Props> = ({ ...rest }) => {
-  const { colors } = useTheme();
+function Input({
+  title,
+  variant = "SIMPLE",
+  children,
+  widthPercent,
+  ...rest
+}: Props) {
+  const { Colors } = useTheme();
+
+  const WIDTH_PERCENT: StyleProp<ViewStyle> = {
+    width: widthPercent ? `${widthPercent}%` : "100%",
+  };
+
   return (
-    <Container>
+    <Container style={WIDTH_PERCENT}>
+      {children && children}
       <InputText
-        placeholderTextColor={colors.inputplacholder}
+        placeholderTextColor={Colors.Textcolor[500]}
         autoCapitalize="none"
         autoCorrect={false}
         {...rest}
       />
     </Container>
   );
-};
+}
+
+interface IconProps {
+  name: keyof typeof Ionicons.glyphMap;
+  size?: number;
+  color?: string;
+}
+
+function Icon({ name, color = "#3a3a3a", size = 24 }: IconProps) {
+  return (
+    <ContainerIcon>
+      <Ionicons name={name} size={size} color={color} />
+    </ContainerIcon>
+  );
+}
+
+Input.Icone = Icon;
 
 export { Input };
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -2,14 +2,24 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex-direction: row;
-  align-items: center;
   border-width: 1px;
-  border-radius: ${(props) => props.theme.size.sm};
-  background-color: ${(props) => props.theme.colors.input};
+  border-radius: ${(props) => props.theme.Size.sm};
+  background-color: ${(props) => props.theme.Colors.Background[100]};
 `;
 export const InputText = styled.TextInput`
   flex: 1;
   padding: 10px;
-  font-size: ${(props) => props.theme.size.md};
-  color: ${(props) => props.theme.colors.black};
+  font-size: ${(props) => props.theme.Size.md};
+  color: ${(props) => props.theme.Colors.Textcolor[700]};
+`;
+export const ContainerIcon = styled.View`
+  width: 60px;
+  align-items: center;
+  justify-content: center;
+  border-right-width: 1px;
+`;
+export const Title = styled.Text`
+  margin-left: ${(props) => props.theme.Size.xs};
+  font-size: ${(props) => props.theme.Size.lg};
+  color: ${(props) => props.theme.Colors.Textcolor[700]};
 `;
