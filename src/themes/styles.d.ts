96723d311044df12218f15f41ca36471869d30b4
doulgs <douglasdsdomiciano@hotmail.com>
2024-04-06 00:51:15 -0300

    Ajustes no ThemeType

--- src/themes/styles.d.ts
+++ src/themes/styles.d.ts
@@ -4,35 +4,35 @@ declare module "styled-components/native" {
   export interface DefaultTheme {
     Colors: {
       Primary: {
-        300: "string";
-        500: "string";
-        800: "string";
+        300: string;
+        500: string;
+        800: string;
       };
       Secondary: {
-        300: "string";
-        500: "string";
-        700: "string";
+        300: string;
+        500: string;
+        700: string;
       };
       Textcolor: {
-        300: "string";
-        500: "string";
-        700: "string";
+        300: string;
+        500: string;
+        700: string;
       };
       Background: {
-        50: "string";
-        100: "string";
-        200: "string";
-        300: "string";
-        400: "string";
-        500: "string";
-        600: "string";
-        700: "string";
-        800: "string";
-        900: "string";
+        50: string;
+        100: string;
+        200: string;
+        300: string;
+        400: string;
+        500: string;
+        600: string;
+        700: string;
+        800: string;
+        900: string;
       };
-      Success: "string";
-      Warning: "string";
-      Error: "string";
+      Success: string;
+      Warning: string;
+      Error: string;
     };
 
     Size: {
