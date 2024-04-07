728b905a2e60cfe41f6fac90077268e4977a371b
doulgs <douglasdsdomiciano@hotmail.com>
2024-04-07 02:07:39 -0300

    Criacao do Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,53 @@
-import React from "react";
+import React, { ReactNode } from "react";
+import {
+  Container,
+  Content,
+  ContentText,
+  Title,
+  SubTitle,
+  ContainerInfo,
+  TitleInfo,
+} from "./styles";
+import { Octicons } from "@expo/vector-icons";
+import { IconLogo } from "../../assets/icons/Icon-Logo";
 
-import { Container } from "./styles";
-
-function Header() {
-  return <Container></Container>;
+interface HeaderProps {
+  nameUser: string | undefined;
+  children: ReactNode;
 }
+
+function Header({ nameUser, children }: HeaderProps) {
+  return (
+    <>
+      <Container>
+        <Content>
+          <ContentText>
+            <Title>Olá, {nameUser}</Title>
+            <SubTitle>Mantenha suas contas em dia</SubTitle>
+          </ContentText>
+          <Octicons name="gear" size={24} color="white" />
+        </Content>
+      </Container>
+      {children}
+    </>
+  );
+}
+
+interface HeaderInfoProps {
+  qtdContas?: number;
+}
+
+function HeaderInfo({ qtdContas = 0 }: HeaderInfoProps) {
+  return (
+    <ContainerInfo>
+      <IconLogo color1="#fff" />
+      <TitleInfo>
+        Você tem {qtdContas} contas {"\n"} cadastrados à serem pagas
+      </TitleInfo>
+    </ContainerInfo>
+  );
+}
+
+Header.Info = HeaderInfo;
 
 export { Header };
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,44 @@
 import styled from "styled-components/native";
+import { StatusBar } from "react-native";
 
-export const Container = styled.View``;
+const STATUS_BAR_HEIGHT = StatusBar.currentHeight;
+
+export const Container = styled.View`
+  flex: 1;
+  max-height: ${185 + (STATUS_BAR_HEIGHT ?? 0)}px;
+  background-color: ${(props) => props.theme.Colors.Background[900]};
+`;
+export const Content = styled.View`
+  flex: 1;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin: ${(props) => props.theme.Size.lg};
+`;
+export const ContentText = styled.View``;
+export const Title = styled.Text`
+  font-weight: 600;
+  font-size: ${(props) => props.theme.Size.lg};
+  color: ${(props) => props.theme.Colors.Textcolor[300]};
+`;
+export const SubTitle = styled.Text`
+  font-size: ${(props) => props.theme.Size.md};
+  color: ${(props) => props.theme.Colors.Textcolor[300]};
+`;
+export const ContainerInfo = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  top: -40px;
+  max-height: 80px;
+  padding: 0 40px;
+  margin: 0 ${(props) => props.theme.Size.lg};
+  border-radius: ${(props) => props.theme.Size.md};
+  background-color: ${(props) => props.theme.Colors.Gray[700]};
+`;
+export const TitleInfo = styled.Text`
+  text-align: center;
+  font-size: ${(props) => props.theme.Size.md};
+  color: ${(props) => props.theme.Colors.Textcolor[300]};
+`;
